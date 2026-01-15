b69f3c269605340cfbfc220620558303e83ddac4
remnantcjy <remnantcjy@gmail.com>
2026-01-15 15:11:02 +0900

    Initial commit

---
+++ src/main/java/com/zero9platform/Zero9PlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zero9platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zero9PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zero9PlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero9platform/Zero9PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero9platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zero9PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
