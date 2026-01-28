305c94858500c47a5fa2bac668f7712fa0c91514
sungwan <uhk561@naver.com>
2026-01-28 15:33:51 +0900

    ✨ feat: 공지사항(NOTICE) 및 이벤트(EVENT) 피드 타입 추가

--- src/main/java/com/zero9platform/common/enums/FeedType.java
+++ src/main/java/com/zero9platform/common/enums/FeedType.java
@@ -6,5 +6,7 @@ public enum FeedType {
     DEADLINE,   // 마감
     LOW_STOCK,  // 재고
     SOON,    // 예정
-    SOLD_OUT // 품절
+    SOLD_OUT, // 품절
+    NOTICE,     // 공지사항 (전체 공지)
+    EVENT       // 이벤트 (프로모션, 할인 등)
 }
