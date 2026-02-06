043e57f1f8c39e3e6cd00affad5acfb798355c9c
sungwan <uhk561@naver.com>
2026-02-06 18:19:33 +0900

    📝 docs(피드타입 enum 메시지 수정):

--- src/main/java/com/zero9platform/common/enums/FeedType.java
+++ src/main/java/com/zero9platform/common/enums/FeedType.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum FeedType {
     // [%s] 형식 지정자 - 주차공간
     POPULAR("[%s] 상품이 현재 많은 관심을 받고 있습니다.", false),    // 찜
-    PAYMENT("[%s] 상품의 새로운 주문이 접수되었습니다.", true),    // 결제
-    DEADLINE("마감 안내: [%s] 상품의 마감이 곧 종료됩니다.", false),   // 마감
+    PAYMENT("[%s] 상품에 대해 최근 %d명이 주문 중입니다!", false),    // 결제
+    DEADLINE("마감 안내: [%s] 상품의 판매가 곧 종료됩니다.", false),   // 마감
     LOW_STOCK("[%s] 상품의 잔여 재고가 얼마 남지 않았습니다.", false),  // 재고
     SOON("[%s] 상품이 곧 오픈 예정입니다.", false),    // 예정
     SOLD_OUT("[%s] 상품이 재고 소진으로 인하여 판매 종료되었습니다.", false), // 품절
@@ -26,7 +26,7 @@ public enum FeedType {
     /**
      * 주차공간에 들어갈 실제 내용
      */
-    public String toMessage(String content) {
-        return String.format(this.messageFormat, content);
+    public String toMessage(Object... args) {
+        return String.format(this.messageFormat, args);
     }
 }
